52ceea585d5a9285841fc29d7bb629b184daf96e
Oskar Kunicki <okunicki@objectivity.co.uk>
2021-09-29 09:06:20 +0200

    init comit

---
+++ src/main/java/co/uk/objectivity/CarsRental/CarsRentalApplication.java
@@ -0,0 +1,13 @@
+package co.uk.objectivity.CarsRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsRentalApplication.class, args);
+	}
+
+}
